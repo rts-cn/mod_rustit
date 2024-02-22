94cd7fdddba082575bcb66c56adcb1dfc870ebcb
Zhubo <Zhubo@rts.cn>
2024-02-22 15:37:57 +0800

    remove derive serde serialize

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
         .out_dir("src/zrs/")
         .compile(&["proto/pb.proto"], &["proto"])
         .unwrap();
