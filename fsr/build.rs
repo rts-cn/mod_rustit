a081911d0b4e03b48c13607e4995af8030ddfb03
Zhubo <Zhubo@rts.cn>
2024-02-23 15:28:17 +0800

    Fixed shared libraries directory

--- fsr/build.rs
+++ fsr/build.rs
@@ -2,19 +2,6 @@
 fn build() {
     let headers_path_str = "/usr/local/freeswitch/include/freeswitch/switch.h";
 
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search={}", "/usr/local/freeswitch/lib");
-
-    // Tell cargo to look for shared libraries  irectory
-    println!(
-        "cargo:rustc-link-arg={}",
-        "-Wl,-rpath=/usr/local/freeswitch/lib"
-    );
-
-    // Tell cargo to tell rustc to link our `freeswitch` library. Cargo will
-    // automatically know it must look for a `libfreeswitch.a` file.
-    println!("cargo:rustc-link-lib=freeswitch");
-
     // Tell cargo to invalidate the built crate whenever the header changes.
     println!("cargo:rerun-if-changed={}", headers_path_str);
 
@@ -47,6 +34,19 @@ fn build() {
 }
 
 fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+    println!("cargo:rustc-link-search={}", "/usr/local/freeswitch/lib");
+
+    // Tell cargo to look for shared libraries  irectory
+    println!(
+        "cargo:rustc-link-arg={}",
+        "-Wl,-rpath=/usr/local/freeswitch/lib"
+    );
+
+    // Tell cargo to tell rustc to link our `freeswitch` library. Cargo will
+    // automatically know it must look for a `libfreeswitch.a` file.
+    println!("cargo:rustc-link-lib=freeswitch");
+
     #[cfg(all(feature = "codegen"))]
     build();
 }
