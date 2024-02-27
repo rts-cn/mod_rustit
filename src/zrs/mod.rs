2f20e3095b4da2f40c627567be6c7edd8477f527
Zhubo <Zhubo@rts.cn>
2024-02-27 15:29:13 +0800

    Fix  deadlock

--- src/zrs/mod.rs
+++ src/zrs/mod.rs
@@ -1,7 +1,7 @@
 use fsr::*;
 use lazy_static::lazy_static;
 use md5;
-use std::sync::Mutex;
+use std::sync::RwLock;
 use std::thread;
 use tokio::sync::broadcast;
 use tokio::sync::mpsc;
@@ -9,6 +9,32 @@ use tonic::{Request, Response, Status};
 
 include!("pb.rs");
 include!("service.rs");
+
+pub struct Zrs {
+    _ev_rx: broadcast::Receiver<Event>,
+    ev_tx: broadcast::Sender<Event>,
+    done: Option<broadcast::Sender<u8>>,
+    apply_inbound_acl: Option<String>,
+    password: Option<String>,
+    threads: i32,
+}
+impl Zrs {
+    fn new() -> Zrs {
+        let (tx, rx) = broadcast::channel::<Event>(16);
+        Zrs {
+            ev_tx: tx,
+            _ev_rx: rx,
+            done: None,
+            apply_inbound_acl: None,
+            password: None,
+            threads: 0,
+        }
+    }
+}
+
+lazy_static! {
+    static ref G_ZRS: RwLock<Zrs> = RwLock::new(Zrs::new());
+}
 
 impl Event {
     pub fn from(e: &fsr::Event) -> Event {
@@ -48,147 +74,95 @@ impl SystemStatus {
     }
 }
 
-pub struct Server {
-    pub bind_uri: String,
-    pub register_uri: String,
-    pub secret: String,
-    pub apply_inbound_acl: String,
-}
-
-pub struct Zrs {
-    _ev_rx: broadcast::Receiver<Event>,
-    ev_tx: broadcast::Sender<Event>,
-    done: Option<broadcast::Sender<u8>>,
-    apply_inbound_acl: Option<String>,
-    password: Option<String>,
-    threads: Vec<std::thread::JoinHandle<()>>,
-}
-
-impl Zrs {
-    fn new() -> Zrs {
-        let (tx, rx) = broadcast::channel::<Event>(16);
-        Zrs {
-            ev_tx: tx,
-            _ev_rx: rx,
-            done: None,
-            apply_inbound_acl: None,
-            password: None,
-            threads: Vec::new(),
+fn check_auth(req: Request<()>) -> Result<Request<()>, Status> {
+    let remote_addr = req.remote_addr();
+    if let Some(remote_addr) = remote_addr {
+        let remote_addr_str = remote_addr.ip().to_string();
+        let apply_inbound_acl: Option<String> = G_ZRS.read().unwrap().apply_inbound_acl.clone();
+        if fsr::check_acl(&remote_addr_str, &apply_inbound_acl.unwrap()) {
+            return Ok(req);
         }
     }
 
-    fn check_auth(req: Request<()>) -> Result<Request<()>, Status> {
-        let remote_addr = req.remote_addr();
-        if let Some(remote_addr) = remote_addr {
-            let remote_addr_str = remote_addr.ip().to_string();
-            let apply_inbound_acl = G_ZRS.lock().unwrap().apply_inbound_acl.clone();
-            if fsr::check_acl(&remote_addr_str, &apply_inbound_acl.unwrap()) {
-                return Ok(req);
+    let authorization = req.metadata().get("authorization");
+    match authorization {
+        Some(t) => {
+            let password = G_ZRS.read().unwrap().password.clone();
+            let password = password.unwrap();
+            let digest = format!("bearer {:x}", md5::compute(password));
+            let token = t.to_str().unwrap();
+            if digest.eq_ignore_ascii_case(token) {
+                Ok(req)
+            } else {
+                Err(Status::unauthenticated(
+                    "authentication failure wrong password",
+                ))
             }
         }
-
-        let authorization = req.metadata().get("authorization");
-        match authorization {
-            Some(t) => {
-                let password = G_ZRS.lock().unwrap().password.clone();
-                let password = password.unwrap();
-                let digest = format!("bearer {:x}", md5::compute(password));
-                let token = t.to_str().unwrap();
-                if digest.eq_ignore_ascii_case(token) {
-                    Ok(req)
-                } else {
-                    Err(Status::unauthenticated(
-                        "authentication failure wrong password",
-                    ))
-                }
-            }
-            _ => Err(Status::unauthenticated("No valid auth token")),
-        }
-    }
-
-    #[tokio::main]
-    async fn tokio_main(addr: String, password: String, acl: String) {
-        let addr = addr.clone();
-        let addr = addr
-            .parse::<std::net::SocketAddr>()
-            .expect("Unable to parse grpc socket address");
-
-        G_ZRS.lock().unwrap().password = Some(password);
-        G_ZRS.lock().unwrap().apply_inbound_acl = Some(acl);
-
-        let (tx, mut rx) = broadcast::channel::<u8>(1);
-        let f = async move {
-            let _ = rx.recv().await;
-        };
-
-        G_ZRS.lock().unwrap().done = Some(tx.clone());
-
-        let service = Service {
-            tx: G_ZRS.lock().unwrap().ev_tx.clone(),
-        };
-
-        debug!("Start zrs rpc service {}", addr);
-        let ret = tonic::transport::Server::builder()
-            .add_service(zrs_server::ZrsServer::with_interceptor(
-                service,
-                Self::check_auth,
-            ))
-            .serve_with_shutdown(addr, f)
-            .await;
-        match ret {
-            Err(e) => {
-                error!("Couldn't start zrpc sever, {}", e);
-            }
-            Ok(_) => {
-                notice!("zrs rpc sever done");
-            }
-        }
-    }
-
-    fn broadcast(&self, ev: Event) {
-        let ret = self.ev_tx.send(ev);
-        match ret {
-            Err(e) => {
-                error!("{}", e);
-            }
-            _ => {
-                // error!("{}", "Event broadcast OK");
-            }
-        }
-    }
-
-    fn done(&mut self) {
-        let _ = self.done.clone().unwrap().send(1);
-        while let Some(handle) = self.threads.pop() {
-            let r = handle.join();
-            match r {
-                Ok(_) => (),
-                Err(_) => {
-                    debug!("Couldn't join on the associated thread");
-                }
-            }
-        }
-        debug!("zrs rpc service thread shutdown.");
-    }
-
-    fn serve(&mut self, addr: String, password: String, acl: String) {
-        let h = thread::spawn(|| Self::tokio_main(addr, password, acl));
-        self.threads.push(h);
+        _ => Err(Status::unauthenticated("No valid auth token")),
     }
 }
 
-lazy_static! {
-    static ref G_ZRS: Mutex<Zrs> = Mutex::new(Zrs::new());
-}
+#[tokio::main]
+async fn tokio_main(addr: String, password: String, acl: String) {
+    let addr = addr.clone();
+    let addr = addr
+        .parse::<std::net::SocketAddr>()
+        .expect("Unable to parse grpc socket address");
 
-pub fn shutdown() {
-    G_ZRS.lock().unwrap().done()
+    G_ZRS.write().unwrap().password = Some(password);
+    G_ZRS.write().unwrap().apply_inbound_acl = Some(acl);
+
+    let (tx, mut rx) = broadcast::channel::<u8>(1);
+    let f = async move {
+        let _ = rx.recv().await;
+    };
+
+    G_ZRS.write().unwrap().done = Some(tx.clone());
+
+    let service = Service {
+        tx: G_ZRS.read().unwrap().ev_tx.clone(),
+    };
+
+    debug!("Start zrs rpc service {}", addr);
+    G_ZRS.write().unwrap().threads += 1;
+    let ret = tonic::transport::Server::builder()
+        .add_service(zrs_server::ZrsServer::with_interceptor(service, check_auth))
+        .serve_with_shutdown(addr, f)
+        .await;
+    match ret {
+        Err(e) => {
+            error!("Couldn't start zrpc sever, {}", e);
+        }
+        Ok(_) => {
+            debug!("zrs rpc service thread shutdown.");
+        }
+    }
+    let _ = G_ZRS.write().unwrap().threads -= 1;
 }
 
 pub fn broadcast(ev: Event) {
-    G_ZRS.lock().unwrap().broadcast(ev);
+    let ret = G_ZRS.read().unwrap().ev_tx.send(ev);
+    match ret {
+        Err(e) => {
+            error!("{}", e);
+        }
+        _ => {
+            // error!("{}", "Event broadcast OK");
+        }
+    }
+}
+
+pub fn shutdown() {
+    let _ = G_ZRS.read().unwrap().done.clone().unwrap().send(1);
+    for _ in 0..10 {
+        if G_ZRS.read().unwrap().threads <= 0 {
+            break;
+        }
+        thread::sleep(std::time::Duration::from_millis(10));
+    }
 }
 
 pub fn serve(addr: String, password: String, acl: String) {
-    G_ZRS.lock().unwrap().serve(addr, password, acl);
+    thread::spawn(|| tokio_main(addr, password, acl));
 }
