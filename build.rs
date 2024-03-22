75f417068702ba6f132fb9cb7dd237ebcec58223
Zhubo <Zhubo@rts.cn>
2024-03-22 10:51:32 +0800

    Moving proto to zrs

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(all(feature = "codegen"))]
+    #[cfg(all(feature = "proto"))]
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .out_dir("src/zrs/")
-        .compile(&["proto/pb.proto"], &["proto"])
+        .compile(&["src/zrs/pb.proto"], &["proto"])
         .unwrap();
     Ok(())
 }
