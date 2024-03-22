34e18bedfa961a217c6b42f03948179214b52713
Zhubo <Zhubo@rts.cn>
2024-03-22 17:40:01 +0800

    refactoring grpc

--- src/grpc/mod.rs
+++ src/grpc/mod.rs
@@ -1,7 +1,9 @@
 use fsr::*;
 use lazy_static::lazy_static;
 use md5;
-use std::sync::RwLock;
+use std::ffi::CString;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use tokio::sync::broadcast;
 use tokio::sync::mpsc;
@@ -11,45 +13,65 @@ pub mod event;
 pub mod pb;
 pub mod service;
 
-pub struct Zrs {
-    ev_tx: broadcast::Sender<pb::Event>,
-    done: mpsc::Sender<u8>,
+#[derive(Debug, Clone)]
+struct Profile {
+    pub event_bind_node: u64,
+    pub listen_ip: String,
+    pub listen_port: u16,
+    pub password: String,
+    pub apply_inbound_acl: String,
+    pub enable: bool,
+}
+
+impl Profile {
+    fn new() -> Profile {
+        Profile {
+            event_bind_node: 0,
+            listen_ip: String::from("0.0.0.0"),
+            listen_port: 8202,
+            password: "".to_string(),
+            apply_inbound_acl: "".to_string(),
+            enable: false,
+        }
+    }
 }
 
 struct Global {
-    running: bool,
-    zrs: Option<Zrs>,
+    running: Mutex<bool>,
+    profile: Mutex<Profile>,
+    ev_tx: Mutex<Option<broadcast::Sender<pb::Event>>>,
+    done_tx: Mutex<Option<mpsc::Sender<u8>>>,
 }
+
 impl Global {
     pub fn new() -> Global {
         Global {
-            running: false,
-            zrs: None,
+            profile: Mutex::new(Profile::new()),
+            running: Mutex::new(false),
+            ev_tx: Mutex::new(None),
+            done_tx: Mutex::new(None),
         }
     }
 }
 
 lazy_static! {
-    static ref GOLOBAS: RwLock<Global> = RwLock::new(Global::new());
+    static ref GOLOBAS: Arc<Global> = Arc::new(Global::new());
 }
 
-fn tokio_main(
-    addr: String,
-    password: String,
-    acl: String,
-    tx: broadcast::Sender<pb::Event>,
-    mut rx: broadcast::Receiver<pb::Event>,
-    mut done: mpsc::Receiver<u8>,
-) {
+fn tokio_main(addr: String, password: String, acl: String) {
     let addr = addr.clone();
     let addr = addr
         .parse::<std::net::SocketAddr>()
         .expect("Unable to parse grpc socket address");
 
+    let (tx, mut rx) = broadcast::channel::<pb::Event>(64);
+    let (done_tx, mut done_rx) = mpsc::channel(1);
+
+    *GOLOBAS.done_tx.lock().unwrap() = Some(done_tx);
+    *GOLOBAS.ev_tx.lock().unwrap() = Some(tx.clone());
+
     let f = async {
-        let _ = done.recv().await;
-        done.close();
-        drop(done);
+        let _ = done_rx.recv().await;
     };
 
     let check_auth = move |req: Request<()>| -> Result<Request<()>, Status> {
@@ -84,19 +106,19 @@ fn tokio_main(
             let ret = rx.recv().await;
             match ret {
                 Ok(_) => {}
-                Err(_) => {
+                Err(e) => {
+                    error!("{}", e);
                     break;
                 }
             }
         }
-        drop(rx);
     });
 
     rt.block_on(async {
         let service = service::Service { tx };
         debug!("Start zrs rpc service {}", addr);
         let ret = tonic::transport::Server::builder()
-            .add_service(pb::switch_server::SwitchServer::with_interceptor(
+            .add_service(pb::fs_server::FsServer::with_interceptor(
                 service, check_auth,
             ))
             .serve_with_shutdown(addr, f)
@@ -110,49 +132,95 @@ fn tokio_main(
     });
 }
 
-pub fn broadcast(ev: fsr::Event) {
-    let zrs = &GOLOBAS.read().unwrap().zrs;
-    if let Some(zrs) = zrs {
-        let ret = zrs.ev_tx.send(pb::Event::from(&ev));
-        if let Err(e) = ret {
-            error!("{}", e);
-        }
-    }
-}
-
 pub fn shutdown() {
-    let r = GOLOBAS.read().unwrap();
-    let zrs = &r.zrs;
-    if let Some(zrs) = zrs {
-        let ret = zrs.done.blocking_send(1);
+    let id = GOLOBAS.profile.lock().unwrap().event_bind_node;
+    if id > 0 {
+        fsr::event_unbind(id);
+    }
+
+    let tx = GOLOBAS.done_tx.lock().unwrap().clone();
+    if tx.is_some() {
+        let ret = tx.clone().unwrap().blocking_send(1);
         if let Err(e) = ret {
             error!("{}", e);
         };
     }
-    drop(r);
 
     for _ in 1..20 {
         thread::sleep(std::time::Duration::from_millis(200));
-        if GOLOBAS.read().unwrap().running == false {
+        if *GOLOBAS.running.lock().unwrap() == false {
             break;
         }
     }
-    GOLOBAS.write().unwrap().zrs = None;
 }
 
-pub fn serve(addr: String, password: String, acl: String) {
-    lazy_static::initialize(&GOLOBAS);
-    let (done_tx, done_rx) = mpsc::channel(1);
-    let (tx, rx) = broadcast::channel::<pb::Event>(64);
-    let zrs = Zrs {
-        ev_tx: tx.clone(),
-        done: done_tx,
-    };
-    GOLOBAS.write().unwrap().zrs = Some(zrs);
-    thread::spawn(move || {
-        GOLOBAS.write().unwrap().running = true;
-        tokio_main(addr, password, acl, tx, rx, done_rx);
-        GOLOBAS.write().unwrap().running = false;
-        debug!("zrs rpc service thread shutdown.");
-    });
+pub fn load_config(cfg: switch_xml_t) {
+    unsafe {
+        let tmp_str = CString::new("grpc").unwrap();
+        let settings_tag = fsr::switch_xml_child(cfg, tmp_str.as_ptr());
+        if !settings_tag.is_null() {
+            let tmp_str = CString::new("param").unwrap();
+            let mut param = fsr::switch_xml_child(settings_tag, tmp_str.as_ptr());
+            while !param.is_null() {
+                let tmp_str = CString::new("name").unwrap();
+                let var = fsr::switch_xml_attr_soft(param, tmp_str.as_ptr());
+                let tmp_str = CString::new("value").unwrap();
+                let val = fsr::switch_xml_attr_soft(param, tmp_str.as_ptr());
+
+                let var = fsr::to_string(var);
+                let val = fsr::to_string(val);
+
+                if var.eq_ignore_ascii_case("listen-ip") {
+                    GOLOBAS.profile.lock().unwrap().listen_ip = val;
+                } else if var.eq_ignore_ascii_case("listen-port") {
+                    GOLOBAS.profile.lock().unwrap().listen_port =
+                        val.parse::<u16>().unwrap_or(8202);
+                } else if var.eq_ignore_ascii_case("password") {
+                    GOLOBAS.profile.lock().unwrap().password = val;
+                } else if var.eq_ignore_ascii_case("apply-inbound-acl") {
+                    GOLOBAS.profile.lock().unwrap().apply_inbound_acl = val;
+                } else if var.eq_ignore_ascii_case("enable") {
+                    GOLOBAS.profile.lock().unwrap().enable = switch_true(&val);
+                }
+
+                param = (*param).next;
+            }
+        }
+    }
+}
+
+fn on_event(ev: fsr::Event) {
+    let tx = GOLOBAS.ev_tx.lock().unwrap().clone();
+    if tx.is_some() {
+        let ret = tx.unwrap().send(pb::Event::from(&ev));
+        if let Err(e) = ret {
+            error!("{}", e);
+        }
+    }
+}
+
+pub fn start(m: &fsr::Module, name: &str) {
+    let profile = GOLOBAS.profile.lock().unwrap().clone();
+    if profile.enable {
+        let bind_uri = format!("{}:{:?}", profile.listen_ip, profile.listen_port);
+        let password = profile.password.clone();
+        let acl = profile.apply_inbound_acl.clone();
+        thread::spawn(move || {
+            *GOLOBAS.running.lock().unwrap() = true;
+            tokio_main(bind_uri, password, acl);
+            *GOLOBAS.running.lock().unwrap() = false;
+            debug!("zrs rpc service thread shutdown.");
+        });
+
+        thread::sleep(std::time::Duration::from_millis(200));
+
+        let evnode = fsr::event_bind(
+            m,
+            name,
+            switch_event_types_t::SWITCH_EVENT_ALL,
+            None,
+            on_event,
+        );
+        GOLOBAS.profile.lock().unwrap().event_bind_node = evnode;
+    }
 }
--- src/mod_zrs.rs
+++ src/mod_zrs.rs
@@ -1,66 +1,12 @@
 use fsr::*;
-use lazy_static::lazy_static;
-use std::{ffi::CString, sync::RwLock, thread};
+use std::{ffi::CString, thread};
 use tokio::runtime::Runtime;
 pub mod cdr;
-pub mod xml;
-pub mod storage;
 pub mod grpc;
-
-struct ZrsModule {
-    event_bind_nodes: Vec<u64>,
-    listen_ip: String,
-    listen_port: u16,
-    password: String,
-    apply_inbound_acl: String,
-    enable: bool,
-}
-
-impl ZrsModule {
-    fn new() -> ZrsModule {
-        ZrsModule {
-            event_bind_nodes: Vec::new(),
-            listen_ip: String::from("0.0.0.0"),
-            listen_port: 8202,
-            password: "".to_string(),
-            apply_inbound_acl: "".to_string(),
-            enable: false,
-        }
-    }
-    fn on_event_bind(id: u64) {
-        MODULE.write().unwrap().event_bind_nodes.push(id);
-    }
-    fn shutdown() {
-        xml::shutdown();
-        cdr::shutdown();
-        storage::shutdown();
-        loop {
-            let id = MODULE.write().unwrap().event_bind_nodes.pop();
-            match id {
-                Some(id) => {
-                    debug!("event unbind");
-                    fsr::event_unbind(id);
-                }
-                None => {
-                    break;
-                }
-            }
-        }
-        if MODULE.read().unwrap().enable {
-            grpc::shutdown();
-        }
-    }
-}
+pub mod storage;
+pub mod xml;
 
 const MODULE_NAME: &str = "mod_zrs";
-
-lazy_static! {
-    static ref MODULE: RwLock<ZrsModule> = RwLock::new(ZrsModule::new());
-}
-
-fn on_event(e: fsr::Event) {
-    let _ = grpc::broadcast(e);
-}
 
 fn api_zsr(_session: &fsr::Session, cmd: String, stream: &fsr::Stream) -> fsr::switch_status_t {
     debug!("api zsr:{}", cmd);
@@ -84,39 +30,7 @@ fn do_config() {
             fsr::switch_xml_free(xml);
             return;
         }
-
-        let tmp_str = CString::new("grpc").unwrap();
-        let settings_tag = fsr::switch_xml_child(cfg, tmp_str.as_ptr());
-        if !settings_tag.is_null() {
-            let tmp_str = CString::new("param").unwrap();
-            let mut param = fsr::switch_xml_child(settings_tag, tmp_str.as_ptr());
-            while !param.is_null() {
-                let tmp_str = CString::new("name").unwrap();
-                let var = fsr::switch_xml_attr_soft(param, tmp_str.as_ptr());
-                let tmp_str = CString::new("value").unwrap();
-                let val = fsr::switch_xml_attr_soft(param, tmp_str.as_ptr());
-
-                let var = fsr::to_string(var);
-                let val = fsr::to_string(val);
-
-                let mut m = MODULE.write().unwrap();
-                if var.eq_ignore_ascii_case("listen-ip") {
-                    m.listen_ip = val;
-                } else if var.eq_ignore_ascii_case("listen-port") {
-                    m.listen_port = val.parse::<u16>().unwrap_or(8202);
-                } else if var.eq_ignore_ascii_case("password") {
-                    m.password = val;
-                } else if var.eq_ignore_ascii_case("apply-inbound-acl") {
-                    m.apply_inbound_acl = val;
-                } else if var.eq_ignore_ascii_case("enable") {
-                    m.enable = switch_true(&val);
-                }
-
-                drop(m);
-                param = (*param).next;
-            }
-        }
-
+        grpc::load_config(cfg);
         xml::load_config(cfg);
         cdr::load_config(cfg);
         storage::load_config(cfg);
@@ -126,28 +40,6 @@ fn do_config() {
 
 fn zrs_mod_load(m: &fsr::Module) -> switch_status_t {
     do_config();
-
-    if MODULE.read().unwrap().enable {
-        let id = fsr::event_bind(
-            m,
-            MODULE_NAME,
-            switch_event_types_t::SWITCH_EVENT_ALL,
-            None,
-            on_event,
-        );
-
-        ZrsModule::on_event_bind(id);
-
-        let module = MODULE.read().unwrap();
-        let listen_ip = module.listen_ip.clone();
-        let listen_port = module.listen_port;
-        let bind_uri = format!("{}:{:?}", listen_ip, listen_port);
-        let password = module.password.clone();
-        let acl = module.apply_inbound_acl.clone();
-        drop(module);
-        grpc::serve(bind_uri, password, acl);
-    }
-
     fsr_api!(m, "zsr", "zsr desc", "zsr syntax", api_zsr);
 
     fsr_app!(
@@ -163,12 +55,16 @@ fn zrs_mod_load(m: &fsr::Module) -> switch_status_t {
     xml::start();
     cdr::start();
     storage::start(m, MODULE_NAME);
+    grpc::start(m, MODULE_NAME);
     switch_status_t::SWITCH_STATUS_SUCCESS
 }
 
 fn zrs_mod_shutdown() -> switch_status_t {
-    ZrsModule::shutdown();
-    // Wait for tokio runtime shutdown
+    grpc::shutdown();
+    xml::shutdown();
+    cdr::shutdown();
+    storage::shutdown();
+
     let rt = Runtime::new().unwrap();
     rt.shutdown_timeout(tokio::time::Duration::from_millis(1000));
     thread::sleep(std::time::Duration::from_millis(200));
