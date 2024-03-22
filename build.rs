f859a42195df87ddeac6984910ccf15deebceb25
Zhubo <Zhubo@rts.cn>
2024-03-22 17:37:49 +0800

    No client-side code is generated

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(all(feature = "proto"))]
     tonic_build::configure()
         .build_server(true)
-        .build_client(true)
+        .build_client(false)
         .out_dir("src/grpc/")
         .compile(&["src/grpc/pb.proto"], &["proto"])
         .unwrap();
